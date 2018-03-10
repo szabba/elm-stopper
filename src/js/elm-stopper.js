da5ea4a22bddb188ca4bfdec6a73e520ba360fcf
Karol Marcjan <karol.marcjan@gmail.com>
2018-03-10 19:37:45 +0100

    Try adding the custom-element class Babel plugin

--- src/js/elm-stopper.js
+++ src/js/elm-stopper.js
@@ -3,6 +3,7 @@ import { Elm } from './elm-stopper.elm.js'
 class ElmStopper extends HTMLElement {
     constructor() {
         super()
+        this.attachShadow({ mode: 'open' })
         Elm.Stopper.Main.embed(this.shadowRoot)
     }
 }
